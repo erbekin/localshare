57a4cb243a280f38218d0d89bab39c5339d7e4a3
emre-erbekin <emreerbekin0@gmail.com>
2025-07-31 13:21:39 +0300

    Update main structure and provide cli option to set password

--- src/record.rs
+++ src/record.rs
@@ -1,20 +1,27 @@
+
 use chrono::DateTime;
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+/// Represents a record of a file uploaded to the system.
+///
+/// Each `FileRecord` contains metadata about the file, including a unique identifier,
+/// display name, optional owner, upload timestamp, description, and optional content type.
+///
+/// # Fields
+/// - `id`: Unique identifier for the file, also used as the filename.
+/// - `name`: Original name of the file provided by the user, used for display purposes.
+/// - `by`: Optional owner of the file. Can be `None` if the owner is not specified.
+/// - `uploaded_at`: Timestamp indicating when the file was uploaded.
+/// - `description`: Description of the file.
+/// - `content_type`: Optional MIME type of the file, used to determine how to respond to requests.
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct FileRecord {
-    // unique id and also filename
     pub id: Uuid,
-    // original name from user to display purposes
     pub name: String,
-    // owner can be null
     pub by: Option<String>,
-    // datetime
     pub uploaded_at: DateTime<Utc>,
-    // description string
     pub description: String,
-    // content type to response appropritetly
     pub content_type: Option<String>,
 }
 
